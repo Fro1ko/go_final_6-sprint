f422b4f1a4315eb6cf9c2ca04a37bb9b45e22904
Vladislav Frolov <frolko95@mail.ru>
2026-03-27 19:34:22 +0400

    first commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,36 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func New(logger *log.Logger) *Server {
+	// 1. Создаем роутер
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handlers.IndexHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+
+	httpSrv := &http.Server{
+		Addr:         ":8080",           
+		Handler:      mux,               
+		ErrorLog:     logger,           
+		ReadTimeout:  5 * time.Second,   
+		WriteTimeout: 10 * time.Second,  
+		IdleTimeout:  15 * time.Second,  
+	}
+
+	return &Server{
+		Logger: logger,
+		Server: httpSrv,
+	}
+}
